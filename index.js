4cc9571eaed38e40b655b8d30e9d235c4cb78ba5
Andrew Johnson <drewbey@gmail.com>
2019-04-02 04:23:25 -0600

    Final submission

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
+require ('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const students = requrie('./students.json');
+
 
 const app = express();
+
+app.use(session ({
+    secret: 'richie called',
+    resave: false, 
+    saveUninitialized: false
+}));
+
+app.use( passport.initialize() );
+app.use( passport.session() );
+
+passport.use( new Auth0Strategy({
+    domain: process.env.DOMAIN,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: '/login',
+    scope: 'openid email profile'
+    }, 
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        return done(null, profile);
+    }
+))
+
+passport.serializeUser(( user, done ) => {
+    done(null, { clientID: user.id, email: user.email, name: user._json.name });
+});
+
+passport.deserializeUser(( user, dont ) => {
+    done(null, obj);
+});
+
+//Auth endpoint
+
+app.get('/login', 
+    passport.authenticate('auth0',
+    { successRedirect: '/students', failureRedirect: '/login', connection: 'github' }
+    )
+);
+
+function authenticated( req, res, next) {
+    if (req.user) {
+        next()
+    } else {
+        res.sendStatus(401)
+    }
+}
+
+app.get('/students', authenticated, (req, res, next) => {
+    res.status(200).send(students)
+})
 
 
 const port = 3000;
